9dd49f9f8f40e98c91e32959c60ef079b0888748
Aditya Gupta <adityaguptacse@gmail.com>
2024-04-23 00:55:34 -0700

    X-Display Namr Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLasttName] = useState("");
+  const [fullName, setFullName] = useState("")
+
+  const submitForm = (e) =>{
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const payload = Object.fromEntries(formData) 
+    console.log(payload)
+    setFullName(payload.firstName+" "+payload.lastName)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Full Name Display</h1>
+        <form onSubmit={submitForm}>
+          <label>First Name:
+            <input type='text'
+            name='firstName'
+            required
+            value={firstName}
+            onChange={(e)=>{
+              setFirstName(e.target.value)
+            }}
+            />
+          </label>
+          <br />
+          <label>Last Name:
+            <input type='text'
+            name='lastName'
+            required
+            value={lastName}
+            onChange={(e)=>{
+              setLasttName(e.target.value)
+            }}
+            />
+          </label>
+          <br/>
+          <button type='submit'>Submit</button>
+        </form>
+        
+        {fullName !== ""? <p>FullName: {fullName}</p> : ""}
     </div>
   );
 }
