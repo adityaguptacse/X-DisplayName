ae7dc0a638de3d9dcfe773c89ccfa6fc55f57504
Aditya Gupta <adityaguptacse@gmail.com>
2024-04-24 13:06:25 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,60 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   const [firstName, setFirstName] = useState("");
   const [lastName, setLasttName] = useState("");
-  const [fullName, setFullName] = useState("")
-  const [hasShow, setHasShow] = useState(false)
+  // const [fullName, setFullName] = useState("")
+  const [hasShow, setHasShow] = useState(false);
 
-  const submitForm = (e) =>{
-    e.preventDefault()
-    const formData = new FormData(e.target)
-    const payload = Object.fromEntries(formData) 
-    console.log(payload)
+  const submitForm = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const payload = Object.fromEntries(formData);
+    console.log(payload);
     // setFullName(payload.firstName+" "+payload.lastName)
-    setHasShow(true)
-
-  }
+    setHasShow(true);
+  };
   return (
     <div className="App">
       <h1>Full Name Display</h1>
-        <form onSubmit={submitForm}>
-          <label>First Name:
-            <input type='text'
-            name='firstName'
+      <form onSubmit={submitForm}>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
             required
             value={firstName}
-            onChange={(e)=>{
-              setFirstName(e.target.value)
+            onChange={(e) => {
+              setFirstName(e.target.value);
             }}
-            />
-          </label>
-          <br />
-          <label>Last Name:
-            <input type='text'
-            name='lastName'
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
             required
             value={lastName}
-            onChange={(e)=>{
-              setLasttName(e.target.value)
+            onChange={(e) => {
+              setLasttName(e.target.value);
             }}
-            />
-          </label>
-          <br/>
-          <button type='submit'>Submit</button>   
-          <br/>
-          {/* {fullName !== ""? `Full Name: ${fullName}`: ""} */}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+        <br />
 
-          {hasShow && (
+  
+      </form>
+      {hasShow && (
           <p>
             Full Name: {firstName} {lastName}
           </p>
-          )}
-        </form>
-        
+        )}
     </div>
   );
 }
